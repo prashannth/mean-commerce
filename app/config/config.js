18271d2a2058492a8131dcbd5882754d61ce2cc4
Prashannth Vijayakumar <vprashannth@ymail.com>
2016-07-02 12:35:10 +0530

    db update

--- app/config/config.js
+++ app/config/config.js
@@ -1,6 +1,6 @@
 config = {
     PORT: process.env.PORT || 3000,
-    DB: 'mongodb://admin:meanmarket@ds011495.mlab.com:11495/heroku_3dbf9l41',
+    DB: 'mongodb://root:123456@ds011735.mlab.com:11735/heroku_5qlb2mfv',
     DB_LOCAL: 'mongodb://localhost/mean_shop_db',
     secret: 'thisisverysevuresecretkey',
     mailer: {
